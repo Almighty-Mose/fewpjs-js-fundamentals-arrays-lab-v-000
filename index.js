b1710efd46dab45d7bc0973a5a570476f12d0e7e
Branden Mosley <branden.mosley1337@gmail.com>
2019-10-10 18:21:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // Write your solution here!
 let append, prepend, removeLast, removeFirst;
 append = prepend = removeLast = removeFirst = ["Milo", "Otis", "Garfield"]
+
+append.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
