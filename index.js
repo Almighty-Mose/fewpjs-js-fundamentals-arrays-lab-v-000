e3dbee0d12e602b3af73a4494925765540a0feb6
Branden Mosley <branden.mosley1337@gmail.com>
2019-10-10 18:24:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,3 @@ const append = ['Milo', 'Otis', 'Garfield'];
 const prepend = append;
 const removeLast = append;
 const removeFirst = append;
-console.log(removeFirst)
-append.push("Odie");
-prepend.unshift("Odie");
-removeLast.pop();
-removeFirst.shift();
